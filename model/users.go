2cd29d02259cd1ab5526a171135b5cc04bc61b96
RexndleZero <raihanaditya1506@gmail.com>
2024-07-18 13:51:36 +0700

    Menambahkan model user

---
+++ model/users.go
@@ -0,0 +1,12 @@
+package model 
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Username string             `json:"username,omitempty" bson:"username,omitempty"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" bson:"password,omitempty"`
+}
