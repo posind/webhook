56aff16e7bab9e195200e8de73c52ecf9c15fc42
RexndleZero <raihanaditya1506@gmail.com>
2024-07-25 15:41:54 +0700

    Mengganti encode endpoint login & register

--- controller/endpoint_user.go
+++ controller/endpoint_user.go
@@ -17,26 +17,26 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	var user model.User
-	_ = json.NewDecoder(r.Body).Decode(&user)
+	var userRegister model.User
+	_ = json.NewDecoder(r.Body).Decode(&userRegister)
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userRegister.Password), bcrypt.DefaultCost)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	user.Password = string(hashedPassword)
-	user.ID = primitive.NewObjectID()
+	userRegister.Password = string(hashedPassword)
+	userRegister.ID = primitive.NewObjectID()
 
 	collection := config.Mongoconn.Collection("users")
-	_, err = collection.InsertOne(context.Background(), user)
+	_, err = collection.InsertOne(context.Background(), userRegister)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(userRegister)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -48,19 +48,19 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&loginRequest)
 
 	collection := config.Mongoconn.Collection("users")
-	var user model.User
-	err := collection.FindOne(context.Background(), bson.M{"email": loginRequest.Email}).Decode(&user)
+	var userLogin model.User
+	err := collection.FindOne(context.Background(), bson.M{"email": loginRequest.Email}).Decode(&userLogin)
 	if err != nil {
 		http.Error(w, "Invalid email or password", http.StatusUnauthorized)
 		return
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(userLogin.Password), []byte(loginRequest.Password))
 	if err != nil {
 		http.Error(w, "Invalid email or password", http.StatusUnauthorized)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(userLogin)
 }
