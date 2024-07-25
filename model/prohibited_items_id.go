0d94a954fe4f9c54aa6dfc047b54ce34c49282fe
Sam9100 <mhaitsamia@gmail.com>
2024-07-25 15:25:03 +0700

    fix typo struct

--- model/prohibited_items_id.go
+++ model/prohibited_items_id.go
@@ -5,7 +5,7 @@ import (
 )
 
 type ProhibitedItem_id struct {
-	ID             primitive.ObjectID  `bson:"_id,omitempty" json:"_id,omitempty"`
-	Destinasi      string              `bson:"Destinasi" json:"Destinasi"`
-	BrangTerlarang string              `bson:"Barang Terlarang" json:"Barang Terlarang"`
+	ID              primitive.ObjectID  `bson:"_id,omitempty" json:"_id,omitempty"`
+	Destinasi       string              `bson:"Destinasi" json:"Destinasi"`
+	BarangTerlarang string              `bson:"Barang Terlarang" json:"Barang Terlarang"`
 }
