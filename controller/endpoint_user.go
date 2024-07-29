d40b0e6a3dfc4c741a183121a845a50b594aa936
RexndleZero <raihanaditya1506@gmail.com>
2024-07-29 14:48:04 +0700

    mengubah endpoint login & register

--- controller/endpoint_user.go
+++ controller/endpoint_user.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/gocroot/config"
-	"github.com/gocroot/helper"
 	"github.com/gocroot/helper/atdb"
 	"github.com/gocroot/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -16,33 +15,43 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Metode tidak diizinkan", http.StatusMethodNotAllowed)
 		return
-	} 	
+	}
 
 	var registrationData model.LoginRequest
 
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&registrationData)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "Data tidak valid: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	_, err = atdb.InsertOneDoc(config.DB, "users", registrationData)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Gagal menyimpan data: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	response := map[string]string{"message": "Registrasi berhasil Jang!"}
+	response := map[string]interface{}{
+		"success": true,
+		"message": "Registrasi berhasil Jang!",
+	}
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(response)
 }
 
+
 // GetUser mengambil informasi user dari database berdasarkan email dan password.
 func GetUser(respw http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(respw, "Metode tidak diizinkan", http.StatusMethodNotAllowed)
+		return
+	}
+
 	var loginDetails model.User
 	if err := json.NewDecoder(req.Body).Decode(&loginDetails); err != nil {
-		helper.WriteJSON(respw, http.StatusBadRequest, err.Error())
+		http.Error(respw, "Data tidak valid: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -50,9 +59,17 @@ func GetUser(respw http.ResponseWriter, req *http.Request) {
 	filter := bson.M{"email": loginDetails.Email, "password": loginDetails.Password}
 	login, err := atdb.GetOneDoc[model.User](config.DB, "users", filter)
 	if err != nil {
-		helper.WriteJSON(respw, http.StatusUnauthorized, "Email atau password salah Jang!")
+		http.Error(respw, "Email atau password salah Jang!", http.StatusUnauthorized)
 		return
 	}
 
-	helper.WriteJSON(respw, http.StatusOK, login)
+	response := map[string]interface{}{
+		"success": true,
+		"message": "Login berhasil!",
+		"user":    login,
+	}
+	respw.Header().Set("Content-Type", "application/json")
+	respw.WriteHeader(http.StatusOK)
+	json.NewEncoder(respw).Encode(response)
 }
+
