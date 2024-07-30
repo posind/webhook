770a67ab45bf45c9e2b0cbbf4903a22bfd1cfa28
DwiFirdaus08 <147034299+DwiFirdaus08@users.noreply.github.com>
2024-07-30 14:02:56 +0700

    Update prohibited_items_id.go

--- model/prohibited_items_id.go
+++ model/prohibited_items_id.go
@@ -7,5 +7,5 @@ import (
 type ProhibitedItem_id struct {
 	ID              primitive.ObjectID  `bson:"_id,omitempty" json:"_id,omitempty"`
 	Destinasi       string              `bson:"Destinasi" json:"Destinasi"`
-	BarangTerlarang string              `bson:"Barang Terlarang" json:"Barang Terlarang"`
+	BarangTerlarang string              `bson:"Barang Terlarang" json:"barang_terlarang"`
 }
