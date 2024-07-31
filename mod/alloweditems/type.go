482d643302664b011e6775e5258bb6166031b437
Sam9100 <mhaitsamia@gmail.com>
2024-07-31 11:18:59 +0700

    try fix allowed mod #4 change typo on struct

--- mod/alloweditems/type.go
+++ mod/alloweditems/type.go
@@ -1,11 +1,9 @@
 package alloweditems
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
+import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Item struct {
-    ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-    Destination  string             `bson:"Destination" json:"destination"`
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+    Destination  string             `bson:"Destination" json:"Destination"`
     AllowedItems string             `bson:"Allowed Items" json:"Allowed Items"`
 }
