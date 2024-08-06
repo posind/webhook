a9e11f8e2f71d866b79670cb01bd6cb3c152b252
Sam9100 <mhaitsamia@gmail.com>
2024-08-06 12:01:44 +0700

    add helper

---
+++ helper/kimseok/type.go
@@ -0,0 +1,10 @@
+package kimseok
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Datasets struct {
+	ID       primitive.ObjectID `json:"id" bson:"_id"`
+	Question string             `json:"question" bson:"question"`
+	Answer   string             `json:"answer" bson:"answer"`
+	Origin   string             `json:"origin" bson:"origin"`
+}
