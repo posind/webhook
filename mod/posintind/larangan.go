6f9cc3ee4b08399901dbe2edb0c713a92f40e67d
Sam9100 <mhaitsamia@gmail.com>
2024-08-08 21:51:29 +0700

    fix the mod to work as they should

--- mod/posintind/larangan.go
+++ mod/posintind/larangan.go
@@ -1,158 +1,164 @@
 package posintid
 
 import (
-	"errors"
-	"regexp"
-	"strconv"
-	"strings"
+    "errors"
+    "regexp"
+    "strconv"
+    "strings"
 
-	"github.com/gocroot/helper/atdb"
-	"github.com/whatsauth/itmodel"
-	"github.com/xrash/smetrics"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
+    "github.com/gocroot/helper/atdb"
+    "github.com/whatsauth/itmodel"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
 )
 
-// GetProhibitedItems fetches prohibited items based on the message and database
 func GetProhibitedItems(Pesan itmodel.IteungMessage, db *mongo.Database) (reply string) {
-	keywords := ExtractKeywords(Pesan.Message, nil)
-	country, item, err := GetCountryAndItemFromKeywords(keywords, db)
-	if err != nil {
-		return "Error: " + err.Error()
-	}
-
-	if country == "" {
-		return "Nama negaranya tidak ada di database kita kakak"
-	}
-
-	filter := bson.M{"Destinasi": country}
-	if item != "" {
-		regexPattern := BuildFlexibleRegexWithTypos([]string{item}, db)
-		filter["Barang Terlarang"] = bson.M{"$regex": regexPattern, "$options": "i"}
-	}
-
-	reply, _, err = populateList(db, filter, item)
-	reply = "📚" + reply
-	if err != nil {
-		jsonData, _ := bson.Marshal(filter)
-		return "📚" + strings.Join(keywords, " ") + "|" + country + " : " + err.Error() + "\n" + string(jsonData)
-	}
-	return
+    country, err := GetCountryFromMessage(Pesan.Message, db)
+    var filter bson.M
+    var keyword string
+    if err != nil {
+        countryandkeyword := ExtractKeywords(Pesan.Message, []string{})
+        words := strings.Split(countryandkeyword, " ")
+        var key []string
+        // Iterate through the slice, popping elements from the end
+        for len(words) > 0 {
+            // Join remaining elements back into a string
+            remainingMessage := strings.Join(words, " ")
+            country, err = GetCountryNameLike(db, remainingMessage)
+            if err == nil {
+                break
+            }
+            // Get the last element
+            lastWord := words[len(words)-1]
+            key = append(key, lastWord)
+            // Remove the last element
+            words = words[:len(words)-1]
+        }
+        if len(key) > 0 {
+            keyword = strings.Join(key, " ")
+            filter = bson.M{
+                "Destinasi":      country,
+                "Barang Terlarang": bson.M{"$regex": keyword, "$options": "i"},
+            }
+        } else {
+            filter = bson.M{"Destinasi": country}
+        }
+        reply, _, err = populateList(db, filter, keyword)
+        reply = "💡" + reply
+        if err != nil {
+            if err.Error() == "zero results" {
+                return " diperbolehkan untuk dikirim ke " + country
+            }
+            jsonData, _ := bson.Marshal(filter)
+            return "💡" + countryandkeyword + "|" + country + " : " + err.Error() + "\n" + string(jsonData)
+        }
+        return
+    }
+    if country == "" {
+        return "Nama negara tidak ada kak di database kita"
+    }
+    keyword = ExtractKeywords(Pesan.Message, []string{country})
+    if keyword != "" {
+        filter = bson.M{
+            "Destinasi":      country,
+            "Barang Terlarang": bson.M{"$regex": keyword, "$options": "i"},
+        }
+    } else {
+        filter = bson.M{"Destination": country}
+    }
+    reply, _, err = populateList(db, filter, keyword)
+    reply = "📚" + reply
+    if err != nil {
+        if err.Error() == "zero results" {
+            return "📚" + keyword + " diperbolehkan untuk dikirim ke " + country
+        }
+        jsonData, _ := bson.Marshal(filter)
+        return "📚 " + keyword + "|" + country + " : " + err.Error() + "\n" + string(jsonData)
+    }
+    return
 }
 
-// GetCountryAndItemFromKeywords determines the country and item from the given keywords
-func GetCountryAndItemFromKeywords(keywords []string, db *mongo.Database) (country, item string, err error) {
-	for i := 0; i < len(keywords); i++ {
-		country, err = GetCountryNameLike(db, keywords[i])
-		if err == nil {
-			item = strings.Join(append(keywords[:i], keywords[i+1:]...), " ")
-			return
-		}
-	}
-
-	err = errors.New("nama negaranya mana kak")
-	return
-}
-
-// GetCountryNameLike searches for a country name in the database
-func GetCountryNameLike(db *mongo.Database, country string) (dest string, err error) {
-	filter := bson.M{
-		"Destinasi": bson.M{"$regex": country, "$options": "i"},
-	}
-	itemprohb, err := atdb.GetOneDoc[Item](db, "prohibited_items_id", filter)
-	if err != nil {
-		return
-	}
-	dest = strings.ReplaceAll(itemprohb.Destinasi, "\u00A0", " ")
-	return
-}
-
-// GetItemNameLike searches for an item name in the database
-func GetItemNameLike(db *mongo.Database, item string) (dest string, err error) {
-	filter := bson.M{
-		"Barang Terlarang": bson.M{"$regex": item, "$options": "i"},
-	}
-	itemprohb, err := atdb.GetOneDoc[Item](db, "prohibited_items_id", filter)
-	if err != nil {
-		return
-	}
-	dest = strings.ReplaceAll(itemprohb.BarangTerlarang, "\u00A0", " ")
-	return
-}
-
-// populateList creates a list of prohibited items based on the filter
 func populateList(db *mongo.Database, filter bson.M, keyword string) (msg, dest string, err error) {
-	listprob, err := atdb.GetAllDoc[Item](db, "prohibited_items_id", filter)
-	if err != nil {
-		return "Terdapat kesalahan pada GetAllDoc", "", err
-	}
-	if len(listprob) == 0 {
-		return "Tidak ada barang terlarang yang ditemukan", "", errors.New("zero results")
-	}
-	dest = listprob[0].Destinasi
-	msg = "Ini dia list barang terlarang dari negara *" + dest + "*:\n"
-	if keyword != "" {
-		msg += "kata-kunci:_" + keyword + "_\n"
-	}
-	for i, probitem := range listprob {
-		msg += strconv.Itoa(i+1) + ". " + probitem.BarangTerlarang + "\n"
-	}
-	return
+    listprob, err := atdb.GetAllDoc[Item](db, "prohibited_items_id", filter)
+    if err != nil {
+        return "Terdapat kesalahan pada GetAllDoc", "", err
+    }
+    if len(listprob) == 0 {
+        return "Tidak ada barang terlarang yang ditemukan", "", errors.New("zero results")
+    }
+    dest = listprob[0].Destinasi
+    msg = " Ini dia list barang terlarang dari negara *" + dest + "*:\n"
+    if keyword != "" {
+        msg += "kata-kunci: _" + keyword + "_\n"
+    }
+    for i, probitem := range listprob {
+        msg += strconv.Itoa(i+1) + ". " + probitem.BarangTerlarang + "\n"
+    }
+    return msg, dest, nil
 }
 
-// ExtractKeywords extracts meaningful keywords from a message
-func ExtractKeywords(message string, commonWordsAdd []string) []string {
-	commonWords := []string{"list", "id", "indo", "mymy"}
-	commonWords = append(commonWords, commonWordsAdd...)
-	message = strings.ToLower(message)
-	message = strings.ReplaceAll(message, "\u00A0", " ")
-
-	for _, word := range commonWords {
-		word = strings.ToLower(strings.ReplaceAll(word, "\u00A0", " "))
-		message = strings.ReplaceAll(message, word, "")
-	}
-
-	message = strings.TrimSpace(message)
-	message = regexp.MustCompile(`\s+`).ReplaceAllString(message, " ")
-	keywords := strings.Split(message, " ")
-
-	if len(keywords) > 2 {
-		keywords = keywords[:2]
-	}
-
-	return keywords
+func GetCountryNameLike(db *mongo.Database, country string) (dest string, err error) {
+    filter := bson.M{
+        "Destinasi": bson.M{"$regex": country, "$options": "i"},
+    }
+    itemprohb, err := atdb.GetOneDoc[Item](db, "prohibited_items_id", filter)
+    if err != nil {
+        return
+    }
+    dest = strings.ReplaceAll(itemprohb.Destinasi, "\u00A0", " ")
+    return
 }
 
-// BuildFlexibleRegexWithTypos creates a flexible regex that accounts for typos
-func BuildFlexibleRegexWithTypos(keywords []string, db *mongo.Database) string {
-	var allKeywords []string
-	items, err := atdb.GetAllDoc[Item](db, "prohibited_items_id", bson.M{})
-	if err == nil {
-		for _, item := range items {
-			words := strings.Split(item.BarangTerlarang, " ")
-			allKeywords = append(allKeywords, words...)
-		}
-	}
-
-	var regexBuilder strings.Builder
-	for _, keyword := range keywords {
-		closestKeyword := findClosestKeyword(keyword, allKeywords)
-		regexBuilder.WriteString("(?=.*\\b" + regexp.QuoteMeta(closestKeyword) + "\\b)")
-	}
-	regexBuilder.WriteString(".*")
-	return regexBuilder.String()
+func GetCountryFromMessage(message string, db *mongo.Database) (country string, err error) {
+    // Ubah pesan menjadi huruf kecil
+    lowerMessage := strings.ToLower(message)
+    // Mengganti non-breaking space dengan spasi biasa
+    lowerMessage = strings.ReplaceAll(lowerMessage, "\u00A0", " ")
+    // Hapus spasi berlebih
+    lowerMessage = strings.TrimSpace(lowerMessage)
+    lowerMessage = regexp.MustCompile(`\s+`).ReplaceAllString(lowerMessage, " ")
+    // Mendapatkan nama negara
+    countries, err := atdb.GetAllDistinctDoc(db, bson.M{}, "Destinasi", "prohibited_items_id")
+    if err != nil {
+        return "", err
+    }
+    var strcountry string
+    // Iterasi melalui daftar negara
+    for _, country := range countries {
+        lowerCountry := strings.ToLower(strings.TrimSpace(country.(string)))
+        // Mengganti non-breaking space dengan spasi biasa
+        lowerCountry = strings.ReplaceAll(lowerCountry, "\u00A0", " ")
+        strcountry += lowerCountry + ","
+        if strings.Contains(lowerMessage, lowerCountry) {
+            return country.(string), nil
+        }
+    }
+    return "", errors.New("tidak ditemukan nama negara di pesan berikut:" + lowerMessage + "|" + strcountry)
 }
 
-// findClosestKeyword finds the closest match for a keyword from a list of known words
-func findClosestKeyword(keyword string, allKeywords []string) string {
-	closestKeyword := keyword
-	minDistance := len(keyword) + 1
-	for _, kw := range allKeywords {
-		distance := smetrics.WagnerFischer(keyword, kw, 1, 1, 2)
-		if distance < minDistance {
-			minDistance = distance
-			closestKeyword = kw
-		}
-	}
-	return closestKeyword
+// Fungsi untuk menghilangkan semua kata kecuali keyword yang diinginkan
+func ExtractKeywords(message string, commonWordsAdd []string) string {
+    // Daftar kata umum yang mungkin ingin dihilangkan
+    commonWords := []string{"list", "id", "mymy"}
+
+    // Gabungkan commonWords dengan commonWordsAdd
+    commonWords = append(commonWords, commonWordsAdd...)
+
+    // Ubah pesan menjadi huruf kecil
+    message = strings.ToLower(message)
+
+    // Ganti non-breaking space dengan spasi biasa
+    message = strings.ReplaceAll(message, "\u00A0", " ")
+
+    // Hapus kata-kata umum dari pesan
+    for _, word := range commonWords {
+        word = strings.ToLower(strings.ReplaceAll(word, "\u00A0", " "))
+        message = strings.ReplaceAll(message, word, "")
+    }
+
+    // Hapus spasi berlebih
+    message = strings.TrimSpace(message)
+    message = regexp.MustCompile(`\s+`).ReplaceAllString(message, " ")
+
+    return message
 }
