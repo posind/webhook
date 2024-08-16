3078750c0ea0b4ed74798a37bb12e098c704c364
Sam9100 <mhaitsamia@gmail.com>
2024-08-16 11:48:21 +0700

    add type struct

--- helper/kimseok/type.go
+++ helper/kimseok/type.go
@@ -8,3 +8,31 @@ type Datasets struct {
 	Answer   string             `json:"answer" bson:"answer"`
 	Origin   string             `json:"origin" bson:"origin"`
 }
+
+// Struktur untuk menyimpan nama negara
+type Country struct {
+	NamaNegara  string `json:"namanegara"`
+	CountryName string `json:"countryname"`
+}
+
+// Definisikan struct untuk data MongoDB id
+type DestinasiTerlarang struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Destinasi       string             `bson:"Destinasi" json:"Destinasi"`
+	BarangTerlarang string             `bson:"Barang Terlarang" json:"Barang Terlarang"`
+}
+
+// Definisikan struct untuk data MongoDB en
+type DestinationProhibit struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Destination     string             `bson:"Destination" json:"Destination"`
+	ProhibitedItems string             `bson:"Prohibited Items" json:"Prohibited Items"`
+}
+
+// Struktur untuk berat max
+type MaxWeight struct {
+	ID               primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	DestinasiNegara  string             `bson:"Destinasi Negara" json:"Destinasi Negara"`
+	KodeNegara       string             `bson:"Kode Negara" json:"Kode Negara"`
+	BeratPerKoli	 string 			`bson:"Berat Per Koli" json:"Berat Per Koli"`
+}
