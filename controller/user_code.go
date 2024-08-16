05ca0e6216542fd4f3d0ce964e92e8c2845a3e0f
Sam9100 <mhaitsamia@gmail.com>
2024-08-16 10:01:35 +0700

    update yml

--- controller/user_code.go
+++ controller/user_code.go
@@ -15,7 +15,7 @@ import (
 
 // GetDataUser handles the GET request to fetch user data
 func GetDataUser(respw http.ResponseWriter, req *http.Request) {
-	payload, err := watoken.Decode(config.PublicKey, helper.GetLoginFromHeader(req))
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, helper.GetLoginFromHeader(req))
 	if err != nil {
 		var respn model.Response
 		respn.Status = "Error: Token Tidak Valid"
@@ -53,51 +53,51 @@ func GetDataUser(respw http.ResponseWriter, req *http.Request) {
 //     }
 
     // Fetch the user data from the database based on the phone number
-    docuser, err := atdb.GetOneDoc[model.Profile_user](config.Mongoconn, "user_login_token", primitive.M{"phonenumber": payload.Id})
-    if err != nil {
-        // If the user is not found, create a new user with the payload data
-        docuser.PhoneNumber = payload.Id
-        docuser.Email = payload.Alias
-        helper.WriteJSON(respw, http.StatusNotFound, docuser)
-        return
-    }
+    // docuser, err := atdb.GetOneDoc[model.Profile_user](config.Mongoconn, "user_login_token", primitive.M{"phonenumber": payload.Id})
+    // if err != nil {
+    //     // If the user is not found, create a new user with the payload data
+    //     docuser.PhoneNumber = payload.Id
+    //     docuser.Email = payload.Alias
+    //     helper.WriteJSON(respw, http.StatusNotFound, docuser)
+    //     return
+    // }
 
     // Update the user's name/alias
-    docuser.Email = payload.Alias
+    // docuser.Email = payload.Alias
 
-    // Get QRIS status from the WAAPI using the phone number from the payload
-    hcode, qrstat, err := atapi.Get[model.QRStatus](config.WAAPIGetToken + helper.GetLoginFromHeader(req))
-    if err != nil {
-        helper.WriteJSON(respw, http.StatusMisdirectedRequest, docuser)
-        return
-    }
+//     // Get QRIS status from the WAAPI using the phone number from the payload
+//     hcode, qrstat, err := atapi.Get[model.QRStatus](config.WAAPIGetToken + helper.GetLoginFromHeader(req))
+//     if err != nil {
+//         helper.WriteJSON(respw, http.StatusMisdirectedRequest, docuser)
+//         return
+//     }
 
-    // If the QRIS status is OK and the QR status is not active, generate a new token
-    if hcode == http.StatusOK && !qrstat.Status {
-        docuser.Token, err = watoken.EncodeforHours(docuser.PhoneNumber, docuser.Email, config.PrivateKey, 43830)
-        if err != nil {
-            helper.WriteJSON(respw, http.StatusFailedDependency, docuser)
-            return
-        }
-    } else {
-        // If the QR status is active, reset the LinkedDevice
-        docuser.Token = ""
-    }
+//     // If the QRIS status is OK and the QR status is not active, generate a new token
+//     if hcode == http.StatusOK && !qrstat.Status {
+//         docuser.Token, err = watoken.EncodeforHours(docuser.PhoneNumber, docuser.Email, config.PrivateKey, 43830)
+//         if err != nil {
+//             helper.WriteJSON(respw, http.StatusFailedDependency, docuser)
+//             return
+//         }
+//     } else {
+//         // If the QR status is active, reset the LinkedDevice
+//         docuser.Token = ""
+//     }
 
-    // Replace or update the user's data in the "user" collection
-    _, err = atdb.ReplaceOneDoc(config.Mongoconn, "user", primitive.M{"phonenumber": payload.Id}, docuser)
-    if err != nil {
-        helper.WriteJSON(respw, http.StatusExpectationFailed, docuser)
-        return
-    }
+//     // Replace or update the user's data in the "user" collection
+//     _, err = atdb.ReplaceOneDoc(config.Mongoconn, "user", primitive.M{"phonenumber": payload.Id}, docuser)
+//     if err != nil {
+//         helper.WriteJSON(respw, http.StatusExpectationFailed, docuser)
+//         return
+//     }
 
-    // Respond with the updated user data
-    helper.WriteJSON(respw, http.StatusOK, docuser)
-}
+//     // Respond with the updated user data
+//     helper.WriteJSON(respw, http.StatusOK, docuser)
+// }
 
 // PostDataUser handles the POST request to update user data
 func PostDataUser(respw http.ResponseWriter, req *http.Request) {
-	payload, err := watoken.Decode(config.PublicKey, helper.GetLoginFromHeader(req))
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, helper.GetLoginFromHeader(req))
 	if err != nil {
 		var respn model.Response
 		respn.Status = "Error: Token Tidak Valid"
